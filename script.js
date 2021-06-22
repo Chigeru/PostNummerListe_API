d978e0c4c8d035aa0a6f80bccde84a3934d9b286
Frank <frankjensen156@gmail.com>
2021-06-22 22:10:19 +0200

    Late push

--- script.js
+++ script.js
@@ -8,13 +8,10 @@ fetch(apiSite)
 function findMatches(wordToMatch, zipCodes) {
     return zipCodes.filter(zip => {
         const regex = new RegExp(wordToMatch, 'gi');
-        const regexExactOne = new RegExp("\\b" + wordToMatch +'\\b');
-        const regexExactList = new RegExp("\\b" + wordToMatch +'\\b', 'gi');
+        const regexExactList = new RegExp("^" + wordToMatch, 'gi');
 
-            
-            return zip.nr.match(regexExactOne) || 
-            zip.navn.match(regex);
-        
+
+        return zip.nr.match(regexExactList) || zip.navn.match(regex);
     })
 }
 
@@ -23,18 +20,26 @@ function displayMatches() {
     const html = matchArray.map(zip => {
         const regex = new RegExp(this.value, 'gi');
 
-        let zipUnits = `<div class="zipListUnit"> <span>${zip.nr} ${zip.navn}</span><div>`; 
+        let zipUnits = `<div class="zipListUnit"> <span>${zip.nr} ${zip.navn}</span><div>`;
         zip.kommuner.forEach(element => {
-            zipUnits = zipUnits +`<span>${element.kode} ${element.navn}</span><br/>`
+            zipUnits = zipUnits + `<span>${element.kode}</span><span> ${element.navn}<br/></span>`
         });
-        
-        zipUnits = zipUnits + `</div></div>` ;
+
+        zipUnits = zipUnits + `</div></div>`;
         return zipUnits;
     }).join('');
-    suggenstions.innerHTML = html;
+    if (html == 0) {
+        suggenstions.innerHTML = '<p>No data</p>'
+        columnName.innerHTML = "";
+    } else {
+        suggenstions.innerHTML = html;
+        columnName.innerHTML = "<div><span>Postnummer & by</span></div><div><span>Kommune nummer & navn</span></div>";
+    }
 }
 const searchInput = document.querySelector('.search');
-const suggenstions = document.querySelector('.holderList');
+const suggenstions = document.querySelector('.list');
+const columnName = document.querySelector('.indhold');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+
